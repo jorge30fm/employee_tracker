398ac369e81bf5e13e15803cc29ea052d3f93c07
Jorge Felix <jorge30fm@gmail.com>
2022-07-03 19:38:42 -0400

    populate tables with data in seeds.sql for testing

--- db/schema.sql
+++ db/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE departments (
 CREATE TABLE roles (
         id INTEGER AUTO_INCREMENT PRIMARY KEY,
         title VARCHAR(30) NOT NULL,
-        salary DECIMAL NOT NULL,
+        salary DECIMAL (7, 2) NOT NULL,
         department_id INTEGER,
 
         CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
